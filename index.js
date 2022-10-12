5e9a775319b4c79366be97213f12623bcf7dd07e
Imravirathore <ravirathore672@gmail.com>
2022-10-12 15:58:51 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+// Play Audio for Cow
+
+var cow_box = document.getElementById("cow_box")
+var cow_audio = document.getElementById("cow_audio"); 
+
+
+
+cow_box.addEventListener('click',function(){
+
+   
+    cow_audio.play(); 
+     
+
+})
+
+cow_box.addEventListener('dblclick',function(){
+
+   
+    cow_audio.pause(); 
+ 
+
+})
+
+// Play Audio for Elephant
+
+var elephant_box = document.getElementById("elephant_box")
+var elephant_audio = document.getElementById("elephant_audio"); 
+
+
+
+elephant_box.addEventListener('click',function(){
+
+   
+    elephant_audio.play(); 
+     
+
+})
+
+elephant_box.addEventListener('dblclick',function(){
+
+   
+    elephant_audio.pause(); 
+ 
+
+})
+
+// Play Audio for Lion
+
+
+var lion_box = document.getElementById("lion_box")
+var lion_audio = document.getElementById("lion_audio"); 
+
+
+
+lion_box.addEventListener('click',function(){
+
+   
+    lion_audio.play(); 
+     
+
+})
+
+lion_box.addEventListener('dblclick',function(){
+
+   
+    lion_audio.pause(); 
+ 
+
+})
